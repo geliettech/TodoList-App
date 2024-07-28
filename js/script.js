b180ae1b576d898b4e77c0aa3ed166086fdf7a09
julietoge <julietogechi27@gmail.com>
2024-07-28 11:13:33 +0100

    update index.html & script.js

--- js/script.js
+++ js/script.js
@@ -18,65 +18,79 @@ document.addEventListener("DOMContentLoaded", function () {
 
   Form.onsubmit = function (e) {
     e.preventDefault(); // Prevent default form submission
-
-    const Li = document.createElement("li");
-    Li.innerHTML = `<i class="fa-regular fa-circle"></i><span>${Input.value}</span><i class="fa-solid fa-pen-to-square edit"></i><i class="fa-solid fa-trash" style="color: #1c7ed6"></i>`;
-
-    //   Tasks.appendChild(Li);
-    Tasks.prepend(Li); // Prepend the new task to the list
+    const task = Input.value;
+    addTask(task);
 
     Input.value = "";
 
     addBtn.disabled = true;
 
+    // stop form from submitting
+    return false;
+  };
+
+  const addTask = (task) => {
+    const Li = document.createElement("li");
+    Li.innerHTML = `<i class="fa-regular fa-circle"></i><span>${task}</span><i class="fa-solid fa-pen-to-square edit"></i><i class="fa-solid fa-trash" style="color: #1c7ed6"></i>`;
+
+    //   Tasks.appendChild(Li);
+    Tasks.prepend(Li); // Prepend the new task to the list
+
     // Add event listener to complete/uncomplete
     let complete = Li.querySelector(".fa-circle");
-    complete.addEventListener("click", function () {
-      if (Li.style.textDecoration === "line-through") {
-        Li.style.textDecoration = "none";
-        complete.classList.add("fa-circle");
-      } else {
-        Li.style.textDecoration = "line-through";
-        complete.classList.remove("fa-circle");
-        complete.classList.add("fa-circle-check");
-      }
-    });
+    complete.addEventListener("click", () => completeTask(Li, complete));
 
     // Add event listener to delete button
     let deleteBtn = Li.querySelector(".fa-trash");
-    deleteBtn.addEventListener("click", function () {
-      Li.remove();
-    });
+    deleteBtn.addEventListener("click", deleteTask);
 
     // Add event listener to edit button
     let editBtn = Li.querySelector(".fa-pen-to-square");
-    editBtn.addEventListener("click", function () {
-      const currentText = Li.querySelector("span").innerText;
-      editInput = document.createElement("input");
-      editInput.type = "text";
-      editInput.value = currentText;
-      Li.querySelector("span").replaceWith(editInput);
-      // Replace the edit icon with a save icon
-      editBtn.classList.remove("fa-pen-to-square");
-      editBtn.classList.remove("edit");
-      editBtn.classList.add("fa-floppy-disk");
-      // Save the edited text
-      editBtn.addEventListener("click", function saveEdit() {
-        let newText = editInput.value;
-        const newSpan = document.createElement("span");
-        newSpan.textContent = newText;
-        editInput.replaceWith(newSpan);
+    editBtn.addEventListener("click", () => editTask(Li, editBtn));
+  };
 
-        // Restore the edit icon
-        editBtn.classList.remove("fa-floppy-disk");
-        editBtn.classList.add("fa-pen-to-square");
+  const completeTask = (Li, complete) => {
+    if (Li.style.textDecoration === "line-through") {
+      Li.style.textDecoration = "none";
+      complete.classList.add("fa-circle");
+    } else {
+      Li.style.textDecoration = "line-through";
+      complete.classList.remove("fa-circle");
+      complete.classList.add("fa-circle-check");
+    }
+  };
 
-        // Remove the save event listener to avoid adding multiple listeners
-        editBtn.removeEventListener("click", saveEdit);
-      });
+  const deleteTask = (event) => {
+    const del = event.target.parentElement;
+    Tasks.removeChild(del);
+  };
+
+  const editTask = (Li, editBtn) => {
+    const currentText = Li.querySelector("span").innerText;
+    editInput = document.createElement("input");
+    editInput.type = "text";
+    editInput.value = currentText;
+    Li.querySelector("span").replaceWith(editInput);
+
+    // Replace the edit icon with a save icon
+    editBtn.classList.remove("fa-pen-to-square");
+    editBtn.classList.add("fa-floppy-disk");
+
+    // Save the edited text
+    editBtn.addEventListener("click", function saveEdit() {
+      let newText = editInput.value;
+      const newSpan = document.createElement("span");
+      newSpan.textContent = newText;
+      editInput.replaceWith(newSpan);
+
+      // Restore the edit icon
+      editBtn.classList.remove("fa-floppy-disk");
+      editBtn.classList.add("fa-pen-to-square");
+
+      // Remove the save event listener to avoid adding multiple listeners
+      editBtn.removeEventListener("click", saveEdit);
     });
 
-    // stop form from submitting
-    return false;
+    // Store Tasks in Local Storage 
   };
 });
